ac5e424b98c7f287813b070607c2914f9fd9f9ea
Mrochen, David <david.mrochen@mediservbank.de>
2024-11-18 08:39:37 +0100

    Inital commit

---
+++ Banking/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Banking
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Banking/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Banking
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
